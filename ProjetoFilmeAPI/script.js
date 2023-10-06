16e04c2abb99dc56fc08dbdfa346d69d88538288
Anderson Dutra <128000302+AndersonDutra7@users.noreply.github.com>
2023-10-05 21:33:23 -0300

    05.10

--- ProjetoFilmeAPI/script.js
+++ ProjetoFilmeAPI/script.js
@@ -2,17 +2,18 @@ let container = document.querySelector('.container')
 let formulario = document.querySelector('.formulario')
 let filmeInput = document.querySelector('.form-control')
 let filmeInfo = document.querySelector('.filme-info')
-let prevPageButton = document.getElementById('prevPage');
-let nextPageButton = document.getElementById('nextPage');
+let prevPageButton = document.getElementById('prev-page');
+let nextPageButton = document.getElementById('next-page');
+let paginaInicial = 1;
 
-async function getApi(filme) {
-    let request = await fetch(`https://www.omdbapi.com/?s=${filme}&apikey=e6c43dc6`);
+async function getApi(filme, pagina) {
+    let request = await fetch(`https://www.omdbapi.com/?s=${filme}&apikey=e6c43dc6&page=${pagina}`);
     let data = await request.json();
 
     if (data.Response === "True") {
         filmeInfo.innerHTML = "";
 
-        data.Search.forEach(filme => {
+        data.Search.forEach((filme) => {
             filmeInfo.innerHTML += `
                 <div class="movie">
                     <div class="movie-header">
@@ -22,18 +23,26 @@ async function getApi(filme) {
                     <p><img src="${filme.Poster}" alt="${filme.Title}"></p>
                 </div>
             `;
-        <div class="paginacao">
-        <button id="prevPage">Página Anterior</button>
-        <button id="nextPage">Próxima Página</button>
-        </div>
-
         });
     } else {
         filmeInfo.innerHTML = "<p>Filme não encontrado.</p>";
     }
 }
 
-formulario.addEventListener('submit', function(e){
+formulario.addEventListener('submit', function(e) {
     e.preventDefault();
-    getApi(filmeInput.value)
-})
+    paginaInicial = 1;
+    getApi(filmeInput.value, paginaInicial);
+});
+
+prevPageButton.addEventListener('click', function() {
+    if (paginaInicial > 1) {
+        paginaInicial--;
+        getApi(filmeInput.value, paginaInicial);
+    }
+});
+
+nextPageButton.addEventListener('click', function() {
+    paginaInicial++;
+    getApi(filmeInput.value, paginaInicial);
+});
