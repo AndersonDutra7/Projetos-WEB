7310e87653923fe7baab6f7f7a6c0bf686ebcc6e
Anderson Dutra <andersonrdutra7@gmail.com>
2023-10-09 14:04:44 -0300

    09.10

--- MarketPlace/src/js/script.js
+++ MarketPlace/src/js/script.js
@@ -1,14 +1,3 @@
 document.getElementById("btn-login").addEventListener("click", function() {
     window.location.href = "login.html";
 });
-
-
-{/* <button id="redirect-button">Ir para Outra Página</button>
-
-<script>
-    // Adicione um evento de clique ao botão
-    document.getElementById("redirect-button").addEventListener("click", function() {
-        // Redireciona para a página desejada usando uma URL absoluta
-        window.location.href = "https://andersondutra7.github.io/MarketPlace/src/html/registro.html";
-    });
-</script> */}
