827f198cf8b95510fd648f52e0075bf06032a936
Anderson Dutra <andersonrdutra7@gmail.com>
2023-10-21 23:16:42 -0300

    feat: add button, card and header components

--- fsw-store/src/components/ui/header.tsx
+++ fsw-store/src/components/ui/header.tsx
@@ -1,12 +1,22 @@
 import { Card } from "./card";
 import { Button } from "./button";
 import { MenuIcon } from "lucide-react";
+import { ShoppingCartIcon } from 'lucide-react';
+
 
 const Header = () => {
-    return <Card className="flex justify-between">
+    return <Card className="flex justify-between p-[1.875rem] items-center">
 
-        <Button size="icon">
+        <Button size="icon" variant="outline">
             <MenuIcon></MenuIcon>
+        </Button>
+
+        <h1 className="font-semibold text-4xl">
+            <span className="text-primary">FSW</span> Store
+        </h1>
+
+        <Button size="icon" variant="outline">
+            <ShoppingCartIcon></ShoppingCartIcon>
         </Button>
 
     </Card>
