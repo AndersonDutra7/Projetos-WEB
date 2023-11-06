f5f7eebbdf63e1b3a18c0c0348063c625d5a372a
Anderson Dutra <andersonrdutra7@gmail.com>
2023-11-05 23:04:54 -0300

    05.11

--- MarketPlace/src/js/script.js
+++ MarketPlace/src/js/script.js
@@ -1,3 +1,70 @@
 document.getElementById("btn-login").addEventListener("click", function() {
     window.location.href = "html/login.html";
 });
+
+const loginForm = document.getElementById('login-form');
+        loginForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const email = document.getElementById('email').value;
+        const senha = document.getElementById('senha').value;
+
+        const formData = new FormData();
+        formData.append('email', email);
+        formData.append('senha', senha);
+
+        fetch('URL_DA_SUA_API_AQUI', {
+            method: 'POST',
+            body: formData
+            })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Resposta da API:', data);
+                // Faça algo com a resposta da API, como redirecionar o usuário ou exibir uma mensagem
+        })
+        .catch(error => {
+            console.error('Erro ao enviar a solicitação POST:', error);
+    });
+});
+
+const registrationForm = document.getElementById('registration-form');
+
+registrationForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+    const confirmarSenha = document.getElementById('confirmarSenha').value;
+
+    if (senha !== confirmarSenha) {
+        alert('As senhas não coincidem');
+        return;
+            }
+
+            const userData = {
+                nome: nome,
+                email: email,
+                senha: senha
+            };
+
+            fetch('URL_DA_SUA_API_DE_REGISTRO', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userData)
+            })
+            .then(response => {
+                if (response.ok) {
+                    alert('Registro bem-sucedido! Você pode fazer login agora.');
+                    // Redirecionar o usuário para a página de login ou fazer outra ação
+                } else {
+                    alert('Falha no registro. Tente novamente mais tarde.');
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao enviar a solicitação POST:', error);
+            });
+        });
+
