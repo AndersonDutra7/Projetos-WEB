f456837fd05984fa43aa6a3cd1c90249371e271e
Anderson Dutra <andersonrdutra7@gmail.com>
2023-11-06 16:57:04 -0300

    06.11
    Add iamgens de projetos

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* FUNCAO OVERLAY MENU-MOBILE */
 let btnMenu = document.getElementById('btn-menu');
 let menu = document.getElementById('menu-mobile');
 let overlay = document.getElementById('overlay-menu');
@@ -13,3 +14,35 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
+
+/* FUNCAO ENVIO FORMULARIO */
+let form = document.getElementById("formulario");
+let nomeContato = document.getElementById("input-nome");
+let emailContato = document.getElementById("input-mail");
+let foneContato = document.getElementById("input-fone");
+let msgContato = document.getElementById("input-msg");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs(){
+    let nomeContatoValor = nomeContato.value;
+    let emailContatoValor = emailContato.value;
+    let foneContatoValor = foneContato.value;
+    let msgContatoValor = msgContato.value;
+
+    if (!checkEmail(emailContatoValor)) {
+        emailContato.classList.add("error"); //Adicione uma classe de erro ao elemento de email (pode ser estilizada com CSS)
+        emailContato.nextElementSibling.textContent = "Por favor, insira um email válido.";
+        console.log(teste01);
+      }
+}
+
+function checkEmail(emailContatoValor) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        emailContatoValor
+    );
+}
