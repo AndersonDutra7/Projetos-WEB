de413b82093c4d156ae91cdda5076e8600b35641
Anderson Dutra <andersonrdutra7@gmail.com>
2023-11-13 11:52:56 -0300

    13/11
    Atualizado formulario

--- script.js
+++ script.js
@@ -3,46 +3,36 @@ let btnMenu = document.getElementById('btn-menu');
 let menu = document.getElementById('menu-mobile');
 let overlay = document.getElementById('overlay-menu');
 
-btnMenu.addEventListener('click', ()=>{
-    menu.classList.add('abrir-menu')
-})
-
-menu.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
-
-overlay.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
-
-/* FUNCAO ENVIO FORMULARIO */
-const form = document.getElementById("formulario");
-let nomeContato = document.getElementById("input-nome");
-let emailContato = document.getElementById("input-mail");
-let foneContato = document.getElementById("input-fone");
-let msgContato = document.getElementById("input-msg");
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    checkInputs();
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu');
 });
 
-function checkInputs(){
-    let nomeContatoValor = nomeContato.value;
-    let emailContatoValor = emailContato.value;
-    let foneContatoValor = foneContato.value;
-    let msgContatoValor = msgContato.value;
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu');
+});
 
-    if (!checkEmail(emailContatoValor)) {
-        emailContato.classList.add("error"); //Adicione uma classe de erro ao elemento de email (pode ser estilizada com CSS)
-        emailContato.nextElementSibling.textContent = "Por favor, insira um email válido.";
-        console.log(teste01);
-      }
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu');
+});
+
+/* FUNCAO ENVIO FORMULARIO */
+const tel = document.getElementById('input-fone')
+
+tel.addEventListener('keypress', (e) => mascaraTelefone(e.target.value));
+tel.addEventListener('change', (e) => mascaraTelefone(e.target.value));
+
+const mascaraTelefone = (valor) => {
+    valor = valor.replace(/\D/g, "");
+    valor = valor.replace(/^(\d{2})(\d)/g, "($1) $2");
+    valor = valor.replace(/(\d)(\d{4})$/, "$1-$2");
+    tel.value = valor;
 }
 
-function checkEmail(emailContatoValor) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-        emailContatoValor
-    );
-}
+
+
+
+// function checkEmail(emailContatoValor) {
+//     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+//         emailContatoValor
+//     );
+// }
