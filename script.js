fa67f12d6d58e0ec80cb16e43d780c11ee6b09e2
Anderson Dutra <andersonrdutra7@gmail.com>
2023-11-13 16:08:20 -0300

    13/11
    Add subtitulos digitados

--- script.js
+++ script.js
@@ -27,3 +27,8 @@ const mascaraTelefone = (valor) => {
     valor = valor.replace(/(\d)(\d{4})$/, "$1-$2");
     tel.value = valor;
 }
+
+function flipElement() {
+    const button = document.querySelector('.');
+    button.classList.toggle('flipped');
+  }
