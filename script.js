5bf5ccd03ea9adc3b5243ce3219de52f37ae6616
Anderson Dutra <andersonrdutra7@gmail.com>
2023-11-13 14:36:56 -0300

    13/11
    add html sucess

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ overlay.addEventListener('click', () => {
     menu.classList.remove('abrir-menu');
 });
 
-/* FUNCAO ENVIO FORMULARIO */
+/* FUNCAO VALIDAR EMAIL */
 const tel = document.getElementById('input-fone')
 
 tel.addEventListener('keypress', (e) => mascaraTelefone(e.target.value));
@@ -27,12 +27,3 @@ const mascaraTelefone = (valor) => {
     valor = valor.replace(/(\d)(\d{4})$/, "$1-$2");
     tel.value = valor;
 }
-
-
-
-
-// function checkEmail(emailContatoValor) {
-//     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-//         emailContatoValor
-//     );
-// }
