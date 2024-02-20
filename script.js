3b6dd501fef6f04a53e33eb1c99a7e7f747dece9
Anderson Dutra <andersonrdutra7@gmail.com>
2024-02-20 14:15:55 -0300

    Adicionado botão whats

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 /* FUNCAO OVERLAY MENU-MOBILE */
-let btnMenu = document.getElementById('btn-menu');
-let menu = document.getElementById('menu-mobile');
-let overlay = document.getElementById('overlay-menu');
+let btnMenu = document.getElementById("btn-menu");
+let menu = document.getElementById("menu-mobile");
+let overlay = document.getElementById("overlay-menu");
 
-btnMenu.addEventListener('click', () => {
-    menu.classList.add('abrir-menu');
+btnMenu.addEventListener("click", () => {
+  menu.classList.add("abrir-menu");
 });
 
-menu.addEventListener('click', () => {
-    menu.classList.remove('abrir-menu');
+menu.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
 });
 
-overlay.addEventListener('click', () => {
-    menu.classList.remove('abrir-menu');
+overlay.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
 });
 
 /* FUNCAO VALIDAR EMAIL */
-const tel = document.getElementById('input-fone')
+const tel = document.getElementById("input-fone");
 
-tel.addEventListener('keypress', (e) => mascaraTelefone(e.target.value));
-tel.addEventListener('change', (e) => mascaraTelefone(e.target.value));
+tel.addEventListener("keypress", (e) => mascaraTelefone(e.target.value));
+tel.addEventListener("change", (e) => mascaraTelefone(e.target.value));
 
 const mascaraTelefone = (valor) => {
-    valor = valor.replace(/\D/g, "");
-    valor = valor.replace(/^(\d{2})(\d)/g, "($1) $2");
-    valor = valor.replace(/(\d)(\d{4})$/, "$1-$2");
-    tel.value = valor;
-}
+  valor = valor.replace(/\D/g, "");
+  valor = valor.replace(/^(\d{2})(\d)/g, "($1) $2");
+  valor = valor.replace(/(\d)(\d{4})$/, "$1-$2");
+  tel.value = valor;
+};
+
+// FUNCAO FIXAR HEADER
+const header = document.querySelector("header");
+const headerHeight = header.offsetHeight;
+
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > headerHeight) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+});
